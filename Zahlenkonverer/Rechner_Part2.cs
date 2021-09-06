0a13d9c22e03dc3f900f52f64d5ece5a540278ae
Nick Rudak <nick@rudak.eu>
2021-09-06 23:14:43 +0200

    Now everything is done
    except that the most trembling part has too start
    
    TESTS

--- Zahlenkonverer/Rechner_Part2.cs
+++ Zahlenkonverer/Rechner_Part2.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,9 +12,8 @@ namespace Zahlenkonverter
 		{
 			private Dictionary<int, string> NumbersIntToString = new Dictionary<int, string>() { { 0, "0" }, { 1, "1" }, { 2, "2" }, { 3, "3" }, { 4, "4" }, { 5, "5" }, { 6, "6" }, { 7, "7" }, { 8, "8" }, { 9, "9" }, { 10, "A" }, { 11, "B" }, {12, "C" }, {13, "D" }, {14, "E" }, {15, "F" }  };
 			private Dictionary<string, int> NumbersStringToInt = new Dictionary<string, int>() { { "0", 0 }, { "1", 1 }, { "2", 2 }, { "3", 3 }, {"4", 4 }, { "5", 5 }, { "6", 6 }, { "7", 7 }, { "8", 8 }, {"9", 9 }, { "A", 10 }, { "B", 11 }, { "C", 12 }, { "D", 13 }, { "E", 14 }, { "F", 15 } };
-			public string ConverterFunc(string InNumber, int InBase, int OutBase) //i want to applaude to microsoft for giving me state of the art testing tools which are non exitent and have to be pulled from some random corner of the internet which i cant find *applause*
+			public string ConverterFunc(string InNumber, int InBase, int OutBase)
 			{
-				string Number;
 				if (InBase == 10)
 				{
 					return FromBase10(InNumber, OutBase);
@@ -22,7 +22,13 @@ namespace Zahlenkonverter
 				{
 					return "" + ToBase10(InNumber, InBase);
 				}
-				return "shut";
+				else
+				{
+					string Number;
+					Number = "" + ToBase10(InNumber, InBase);
+					Number = FromBase10(Number, OutBase);
+					return Number;
+				}
 			}
 			private int ToBase10(string InNumber, int InBase)
 			{
